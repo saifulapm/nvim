0b272d4ec19ebeb0c91542dabc7257a0203201d7
Saiful Islam <saiful.apm@gmail.com>
2021-10-13 21:52:16 +0600

    Lsp Line Diag Disable

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -49,7 +49,7 @@ fn.sign_define(vim.tbl_map(function(t)
     name = hl,
     text = t.icon,
     texthl = hl,
-    linehl = fmt('%sLine', hl),
+    -- linehl = fmt('%sLine', hl),
   }
 end, diagnostic_types))
 
